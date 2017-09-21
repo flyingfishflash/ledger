e99af0d3f07fdedb9ae6b75991a8704572c10dcb
gregory <rollenwiese@fastmail.net>
2017-09-20 20:47:03 -0400

    spring boot 1.5.7
    - add yahoo finance api maven dependency
    
    - fix jpa property in application properties
    
    - correct field names in account class
    
    - various hierarchial data object model attempts

--- ledger-concept/src/main/java/net/flyingfishflash/ledger/LedgerApplication.java
+++ ledger-concept/src/main/java/net/flyingfishflash/ledger/LedgerApplication.java
@@ -2,11 +2,54 @@ package net.flyingfishflash.ledger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
+import java.util.Iterator;
+
+@EnableAutoConfiguration
+@ComponentScan("net.flyingfishflash.ledger")
 @SpringBootApplication
 public class LedgerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LedgerApplication.class, args);
+/*		
+		Tree tree = new Tree();
+
+        tree.addNode("Root");
+        tree.addNode("Assets", "Root");
+        tree.addNode("Liabilities", "Root");
+        tree.addNode("Current Liabilities", "Liabilities");
+        tree.addNode("Fixed Liabilities", "Liabilities");
+        tree.addNode("Transient Liabilities", "Liabilities");
+        tree.addNode("Income", "Root");
+        tree.addNode("Expense", "Root");
+        tree.addNode("Equity", "Root");
+        tree.addNode("Financial Assets", "Assets");
+        tree.addNode("Fixed Assets", "Assets");
+
+        tree.display("Root");
+
+        System.out.println("n***** DEPTH-FIRST ITERATION *****");
+
+        // Default traversal strategy is 'depth-first'
+        Iterator<Node> depthIterator = tree.iterator("Root");
+
+        while (depthIterator.hasNext()) {
+            Node node = depthIterator.next();
+            System.out.println(node.getIdentifier());
+        }
+
+        System.out.println("n***** BREADTH-FIRST ITERATION *****");
+
+        Iterator<Node> breadthIterator = tree.iterator("Root", TraversalStrategy.BREADTH_FIRST);
+
+        while (breadthIterator.hasNext()) {
+            Node node = breadthIterator.next();
+            System.out.println(node.getIdentifier());
+        }
+*/
 	}
+
 }
---
+++ ledger-concept/src/main/java/net/flyingfishflash/ledger/TreeNode.java
@@ -0,0 +1,83 @@
+package net.flyingfishflash.ledger;
+
+
+//https://coderanch.com/t/629306/Simple-Hibernate-Lazy-Loading
+	
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TreeNode
+{
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   //@GeneratedValue( generator = "increment" )
+   //@GenericGenerator( name = "increment", strategy = "increment" )
+   private long                    _id;
+   @javax.persistence.ManyToOne
+   private TreeNode                _parent;
+   @javax.persistence.OneToMany( mappedBy = "_parent", cascade = javax.persistence.CascadeType.ALL, fetch = javax.persistence.FetchType.EAGER )
+   private java.util.Set<TreeNode> _children = new java.util.HashSet<TreeNode>();
+   private String                  _data;
+ 
+   private TreeNode()
+   {
+   }
+ 
+   private TreeNode( String data )
+   {
+      _parent = null;
+      _data = data;
+   }
+ 
+   private TreeNode( final TreeNode parent,
+                           String   data )
+   {
+      if ( parent == null )
+      {
+         throw new IllegalArgumentException( "parent required" );
+      }
+ 
+      _parent = parent;
+      _data = data;
+      registerInParentsChilds();
+   }
+ 
+   public TreeNode addChild( String data )
+   {
+      return new TreeNode( this,
+                           data );
+   }
+ 
+   public void display( String margin )
+   {
+      System.out.println( margin + _data );
+ 
+      for ( TreeNode child : _children )
+      {
+         child.display( margin + "   " );
+      }
+   }
+ 
+   private void registerInParentsChilds()
+   {
+      _parent._children.add( this );
+   }
+ 
+   //public java.util.List<TreeNode> getChildren()
+   //{
+   //    return java.util.Collections.unmodifiableList( _children );
+   //}
+ 
+   public String getData()
+   {
+      return _data;
+   }
+ 
+   public static TreeNode createRoot( String data )
+   {
+      return new TreeNode( data );
+   }
+}
