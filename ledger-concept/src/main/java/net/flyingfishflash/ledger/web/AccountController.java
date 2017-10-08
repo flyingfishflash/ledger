e2246bf33ebeacc34f4067065ed21701a055ac4e
gregory <rollenwiese@fastmail.net>
2017-10-08 12:35:35 -0400

    add node to tree via ledger/accounts/create

--- ledger-concept/src/main/java/net/flyingfishflash/ledger/web/AccountController.java
+++ ledger-concept/src/main/java/net/flyingfishflash/ledger/web/AccountController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import net.flyingfishflash.ledger.domain.nestedset.Node;
-import net.flyingfishflash.ledger.domain.nestedset.NodeDAOImpl;
+//import net.flyingfishflash.ledger.domain.nestedset.NodeDAOImpl;
 import net.flyingfishflash.ledger.domain.nestedset.NodeService;
 
 
@@ -25,7 +25,7 @@ public class AccountController {
 	
     @Autowired
     private NodeService service;
-
+    
     // display table of accounts with options to create/update/delete
     @RequestMapping(value = "/ledger/accounts", method = RequestMethod.GET)
     public String nodes(Model model) throws Exception {
@@ -36,40 +36,34 @@ public class AccountController {
         model.addAttribute("node", new Node());
         return "/ledger/accounts/index";
     }
-
+    // TODO Should not show the 'root' account (ID = 1)
     @RequestMapping(value = "/ledger/accounts/create", method = RequestMethod.GET)
-    public String createNode(@RequestParam(name="parentId", defaultValue="1") int parentId, Model model) throws Exception {
-    	logger.info("@RequestMapping: /ledger/accounts/create");
-    	logger.info("RequestParam: " + parentId);
-    	// TODO NullPointer Exception if parentId does not exist in nested set
-    	Node parent = service.findOneById(parentId);
+    public String createNode(@RequestParam(name="parentAccountId", defaultValue="1") int parentAccountId, Model model) throws Exception {
+    	logger.info("@RequestMapping: /ledger/accounts/create (GET)");
+    	logger.info("RequestParam: " + parentAccountId);
+    	// TODO Handle NullPointer Exception if parentId does not exist in nested set
+    	Node parent = service.findOneById(parentAccountId);
     	logger.info("Parent: " + parent.getId() + ": " + parent.getName());
-        //List<Node> nodes = new ArrayList<Node>(service.findWholeTree());
         model.addAttribute("title", "Create Account");
-        model.addAttribute("parent");
+        model.addAttribute("parent", parent);
         model.addAttribute("node", new Node());
-       
         return "/ledger/accounts/create";
     }
     
-    /*
-    	POST http://localhost:8080/ledger/accounts/add
-    	Content-Type: application/x-www-form-urlencoded
-    	currentNodeId=1&method=first
-    */
-    @RequestMapping(value = "/ledger/accounts/add", method = RequestMethod.POST)
-    public String addNode(@ModelAttribute("node") Node node, @RequestParam("currentNodeId") int id, @RequestParam("method") String method) throws Exception {
-        Node parent = service.findOneById(id);
+    // 	POST http://localhost:8080/ledger/accounts/create?parentAccountId=1
+    @RequestMapping(value = "/ledger/accounts/create", method = RequestMethod.POST)
+    public String saveNode(@ModelAttribute("node") Node node, @RequestParam("parentAccountId") int parentAccountId/*, @RequestParam("method") String method*/) throws Exception {
+    	logger.info("@RequestMapping: /ledger/accounts/create (POST)");
+        Node parent = service.findOneById(parentAccountId);
         System.out.println(parent.toString());
         System.out.println(parent.getId());
         System.out.println(parent.getName());
-
+        String method = "last";
         if (method.equals("first")) {
-            service.addChildAsFirst(parent, "new node first child", 99/*node.getName(), node.getValue()*/);
+            service.addChildAsFirst(parent, node.getName(), 99/*node.getName(), node.getValue()*/);
         } else {
-            service.addChildAsLast(parent, "new node last child", 99 /*node.getName(), node.getValue()*/);
+            service.addChildAsLast(parent, node.getName(), 99 /*node.getName(), node.getValue()*/);
         }
-
         return "redirect:/ledger/accounts";
 
     }
