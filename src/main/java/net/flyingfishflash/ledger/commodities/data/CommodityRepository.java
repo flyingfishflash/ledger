0bbb04fd343a6a7dede6f6ced3922e6d7bcaca5a
gregory <rollenwiese@fastmail.net>
2020-02-13 15:08:59 -0500

    add repository and transactional annotations to commodity repository

--- src/main/java/net/flyingfishflash/ledger/commodities/data/CommodityRepository.java
+++ src/main/java/net/flyingfishflash/ledger/commodities/data/CommodityRepository.java
@@ -3,7 +3,11 @@ package net.flyingfishflash.ledger.commodities.data;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional
 public interface CommodityRepository extends PagingAndSortingRepository<Commodity, Long> {
 
   public Optional<Commodity> findByGuid(String guid);
