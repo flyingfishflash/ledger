99f7b6c49b06b20a0653bb87608e25df57a425ac
gregory <rollenwiese@fastmail.net>
2020-11-19 21:13:01 -0500

    move backend to root of project directory

--- scripts/drone-cache-push.sh
+++ scripts/drone-cache-push.sh
@@ -6,11 +6,11 @@ drone_cache="/drone/drone_cache/$1.cache.tar"
 if [ "$1" = "ledger-frontend" ];
 then
   echo "* $1"
-  tar -cf "$drone_cache" /drone/src/src/main/frontend/node_modules/ /drone/src/src/main/frontend/dist/
+  tar -cf "$drone_cache" /drone/src/frontend/node_modules/ /drone/src/frontend/dist/
 else
   if [ "$1" = "ledger-backend" ];
   then
-    tar -cf "$drone_cache" /drone/src/.gradle/ /drone/src/build/ /drone/src/version
+    tar -cf "$drone_cache" /drone/src/.gradle/ /drone/src/version /drone/src/backend/build/
   else
     echo "neither ledger-frontend or ledger-backend was passed as a parameter"
   fi
