5622721eb86c12adee30a820522f1add153a7945
flyingfishflash <flash@flyingfishflash.net>
2022-03-10 18:48:50 -0500

    fix(backend): unknown finding #2

--- backend/src/main/java/net/flyingfishflash/ledger/importer/GncXmlHelper.java
+++ backend/src/main/java/net/flyingfishflash/ledger/importer/GncXmlHelper.java
@@ -8,7 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public abstract class GncXmlHelper {
+public final class GncXmlHelper {
 
   private GncXmlHelper() {
     throw new IllegalStateException("Utility class");
