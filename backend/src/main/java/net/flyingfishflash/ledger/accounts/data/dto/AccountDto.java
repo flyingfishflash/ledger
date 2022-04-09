cb29465d23567d074c157e01094d5e41d16f29da
flyingfishflash <flash@flyingfishflash.net>
2022-04-09 19:42:43 -0400

    fix(backend): sonarlint - account dto
    - resolve finding for rule java:S1104
    - https://rules.sonarsource.com/java/RSPEC-1104

--- backend/src/main/java/net/flyingfishflash/ledger/accounts/data/dto/AccountDto.java
+++ backend/src/main/java/net/flyingfishflash/ledger/accounts/data/dto/AccountDto.java
@@ -12,29 +12,29 @@ import net.flyingfishflash.ledger.accounts.data.AccountType;
 
 public class AccountDto {
 
-  public AccountCategory accountCategory;
+  private AccountCategory accountCategory;
 
-  public AccountType accountType;
-
-  @Size(max = 2048)
-  @Pattern(
-      regexp = "^(?!\\s*$).+",
-      message = "may be null, must not be an empty string, must not consist only of spaces")
-  public String code;
+  private AccountType accountType;
 
   @Size(max = 2048)
   @Pattern(
       regexp = "^(?!\\s*$).+",
       message = "may be null, must not be an empty string, must not consist only of spaces")
-  public String description;
+  private String code;
+
+  @Size(max = 2048)
+  @Pattern(
+      regexp = "^(?!\\s*$).+",
+      message = "may be null, must not be an empty string, must not consist only of spaces")
+  private String description;
 
   @Size(max = 2048)
   @NotBlank
-  public String discriminator;
+  private String discriminator;
 
   @Size(max = 2048)
   @NotBlank
-  public String guid;
+  private String guid;
 
   @NotNull public Boolean hidden;
 
@@ -42,29 +42,29 @@ public class AccountDto {
 
   @Size(max = 2048)
   @NotBlank
-  public String longName;
+  private String longName;
 
   @Size(max = 2048)
   @NotBlank
-  public String name;
+  private String name;
 
   @Size(max = 4096)
   @Pattern(
       regexp = "^(?!\\s*$).+",
       message = "may be null, must not be an empty string, must not consist only of spaces")
-  public String note;
+  private String note;
 
-  @Positive @NotNull public Long parentId;
+  @Positive @NotNull private Long parentId;
 
-  @NotNull public Boolean placeholder;
+  @NotNull private Boolean placeholder;
 
-  @NotNull public Boolean taxRelated;
+  @NotNull private Boolean taxRelated;
 
-  @Positive public Long treeLeft;
+  @Positive private Long treeLeft;
 
-  @Positive public Long treeLevel;
+  @Positive private Long treeLevel;
 
-  @Positive public Long treeRight;
+  @Positive private Long treeRight;
 
   public AccountDto(Account account) {
 
@@ -85,6 +85,142 @@ public class AccountDto {
     this.taxRelated = account.getTaxRelated();
     this.accountCategory = account.getCategory();
     this.accountType = account.getType();
+  }
+
+  public AccountCategory getAccountCategory() {
+    return accountCategory;
+  }
+
+  public void setAccountCategory(AccountCategory accountCategory) {
+    this.accountCategory = accountCategory;
+  }
+
+  public AccountType getAccountType() {
+    return accountType;
+  }
+
+  public void setAccountType(AccountType accountType) {
+    this.accountType = accountType;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getDiscriminator() {
+    return discriminator;
+  }
+
+  public void setDiscriminator(String discriminator) {
+    this.discriminator = discriminator;
+  }
+
+  public String getGuid() {
+    return guid;
+  }
+
+  public void setGuid(String guid) {
+    this.guid = guid;
+  }
+
+  public Boolean getHidden() {
+    return hidden;
+  }
+
+  public void setHidden(Boolean hidden) {
+    this.hidden = hidden;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getLongName() {
+    return longName;
+  }
+
+  public void setLongName(String longName) {
+    this.longName = longName;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getNote() {
+    return note;
+  }
+
+  public void setNote(String note) {
+    this.note = note;
+  }
+
+  public Long getParentId() {
+    return parentId;
+  }
+
+  public void setParentId(Long parentId) {
+    this.parentId = parentId;
+  }
+
+  public Boolean getPlaceholder() {
+    return placeholder;
+  }
+
+  public void setPlaceholder(Boolean placeholder) {
+    this.placeholder = placeholder;
+  }
+
+  public Boolean getTaxRelated() {
+    return taxRelated;
+  }
+
+  public void setTaxRelated(Boolean taxRelated) {
+    this.taxRelated = taxRelated;
+  }
+
+  public Long getTreeLeft() {
+    return treeLeft;
+  }
+
+  public void setTreeLeft(Long treeLeft) {
+    this.treeLeft = treeLeft;
+  }
+
+  public Long getTreeLevel() {
+    return treeLevel;
+  }
+
+  public void setTreeLevel(Long treeLevel) {
+    this.treeLevel = treeLevel;
+  }
+
+  public Long getTreeRight() {
+    return treeRight;
+  }
+
+  public void setTreeRight(Long treeRight) {
+    this.treeRight = treeRight;
   }
 
   @Override
