294d1b016723136a7e5fe8936fa71d1dbf6b76b1
flyingfishflash <flash@flyingfishflash.net>
2022-04-09 17:05:09 -0400

    fix(backend): sonarlint - error response body item
    - refactor into record class
    - https://rules.sonarsource.com/java/RSPEC-6206

--- backend/src/main/java/net/flyingfishflash/ledger/foundation/response/structure/errors/ErrorResponseBodyItem.java
+++ backend/src/main/java/net/flyingfishflash/ledger/foundation/response/structure/errors/ErrorResponseBodyItem.java
@@ -1,21 +1,6 @@
 package net.flyingfishflash.ledger.foundation.response.structure.errors;
 
-public class ErrorResponseBodyItem {
-  private final String reason;
-  private final String message;
-
-  public ErrorResponseBodyItem(final String reason, final String message) {
-    this.reason = reason;
-    this.message = message;
-  }
-
-  public String getReason() {
-    return reason;
-  }
-
-  public String getMessage() {
-    return message;
-  }
+public record ErrorResponseBodyItem(String reason, String message) {
 
   @Override
   public String toString() {
