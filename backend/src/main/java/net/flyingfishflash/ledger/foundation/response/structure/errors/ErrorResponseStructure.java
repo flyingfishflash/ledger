35968b8fdd7fc24003d991664dc6104185d0f6dc
flyingfishflash <flash@flyingfishflash.net>
2022-04-09 17:09:06 -0400

    fix(backend): sonarlint - error response structure
    - suppress warning for java:S1948
    - https://rules.sonarsource.com/java/RSPEC-1948

--- backend/src/main/java/net/flyingfishflash/ledger/foundation/response/structure/errors/ErrorResponseStructure.java
+++ backend/src/main/java/net/flyingfishflash/ledger/foundation/response/structure/errors/ErrorResponseStructure.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponseStructure<T> implements java.io.Serializable {
 
+  @SuppressWarnings("java:S1948")
   @JsonInclude(JsonInclude.Include.NON_NULL)
   private T body;
 
