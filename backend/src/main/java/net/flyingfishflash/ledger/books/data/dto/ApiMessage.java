dfdca889b61f46ee2618684ce0c5780317e8c5e4
flyingfishflash <flash@flyingfishflash.net>
2022-04-27 13:48:21 -0400

    refactor(backend): book api message dto to record from class

--- backend/src/main/java/net/flyingfishflash/ledger/books/data/dto/ApiMessage.java
+++ backend/src/main/java/net/flyingfishflash/ledger/books/data/dto/ApiMessage.java
@@ -1,17 +1,4 @@
 package net.flyingfishflash.ledger.books.data.dto;
 
-public class ApiMessage {
-  private String message;
-
-  public ApiMessage(String message) {
-    this.message = message;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
-}
+/** Record (DTO) class representing a simple API Response */
+public record ApiMessage(String message) {}
