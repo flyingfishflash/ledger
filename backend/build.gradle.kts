45ad24f22b5b5d6f9c61bd6cc69057f5a6a65c51
flyingfishflash <flash@flyingfishflash.net>
2023-02-22 22:57:06 -0500

    build(backend): org.springframework.security:spring-security-test 6.0.2

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.4")
   implementation("org.zalando:jackson-datatype-money:1.3.0")
   runtimeOnly("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
-  testImplementation("org.springframework.security:spring-security-test:6.0.1")
+  testImplementation("org.springframework.security:spring-security-test:6.0.2")
   testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
   errorprone("com.google.errorprone:error_prone_core:2.18.0")
 }
