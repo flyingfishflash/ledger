d2751cb00ab477e586c4d9f972dc8a3758f1339d
flyingfishflash <flash@flyingfishflash.net>
2023-03-11 12:20:13 -0500

    refactor(backend): clean-up http session code

--- backend/src/main/java/net/flyingfishflash/ledger/core/authentication/CustomAuthenticationEntryPoint.java
+++ backend/src/main/java/net/flyingfishflash/ledger/core/authentication/CustomAuthenticationEntryPoint.java
@@ -1,35 +1,27 @@
 package net.flyingfishflash.ledger.core.authentication;
 
-import java.io.IOException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-  private static final Logger logger =
-      LoggerFactory.getLogger(CustomAuthenticationEntryPoint.class);
+  @Qualifier("handlerExceptionResolver") private final HandlerExceptionResolver resolver;
 
-  @Autowired
-  @Qualifier("handlerExceptionResolver") private HandlerExceptionResolver resolver;
+  public CustomAuthenticationEntryPoint(
+      @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+    this.resolver = resolver;
+  }
 
   @Override
   public void commence(
-      HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
-      throws IOException, ServletException {
-
-    logger.info("* Authentication Exception: {}", exception.getLocalizedMessage());
+      HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) {
 
     resolver.resolveException(request, response, null, exception);
   }
--- backend/src/main/java/net/flyingfishflash/ledger/core/authentication/CustomInvalidSessionStrategy.java
+++ backend/src/main/java/net/flyingfishflash/ledger/core/authentication/CustomInvalidSessionStrategy.java
@@ -1,13 +1,8 @@
 package net.flyingfishflash.ledger.core.authentication;
 
-import java.io.IOException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.web.session.InvalidSessionStrategy;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -15,16 +10,8 @@ import net.flyingfishflash.ledger.core.authentication.exceptions.InvalidSessionE
 
 public class CustomInvalidSessionStrategy implements InvalidSessionStrategy {
 
-  private static final Logger logger = LoggerFactory.getLogger(CustomInvalidSessionStrategy.class);
-
   @Override
-  public void onInvalidSessionDetected(HttpServletRequest request, HttpServletResponse response)
-      throws IOException, ServletException {
-
-    logger.debug(
-        "Invalid session requested ({})detected when requesting URI: {}",
-        request.getRequestURI(),
-        request.getRequestedSessionId());
+  public void onInvalidSessionDetected(HttpServletRequest request, HttpServletResponse response) {
 
     throw new InsufficientAuthenticationException(
         "Full authentication is required to access this resource",
