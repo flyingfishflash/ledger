2dc71336995ab27637fa671ad74561ddca14ed40
flyingfishflash <flash@flyingfishflash.net>
2023-04-23 17:40:55 -0400

    refactor(backend): spring-boot 3.0.6

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   java
-  id("org.springframework.boot") version "3.0.5"
+  id("org.springframework.boot") version "3.0.6"
   id("com.diffplug.spotless")
   id("jacoco")
   id("net.ltgt.errorprone") version "3.1.0"
@@ -17,7 +17,7 @@ val ciPlatform: String by rootProject.extra
 val ciPipelineId: String by rootProject.extra
 
 dependencies {
-  val springBootVersion = "3.0.5"
+  val springBootVersion = "3.0.6"
   val springSessionVersion = "3.0.1"
 
   implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
