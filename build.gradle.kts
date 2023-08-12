d4e5c200a188f33b31f25f43464c53fabc776f3f
flyingfishflash <flash@flyingfishflash.net>
2023-08-12 18:02:56 -0400

    build(backend): sonar 4.3.0.3225

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("com.diffplug.spotless") version "6.20.0"
   id("com.github.ben-manes.versions") version "0.46.0"
-  id("org.sonarqube") version "4.0.0.2929"
+  id("org.sonarqube") version "4.3.0.3225"
 }
 
 description =
