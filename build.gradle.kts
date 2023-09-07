12962a068cc448989ea2be2d85da9d03f16450fa
flyingfishflash <flash@flyingfishflash.net>
2023-09-07 17:09:48 -0400

    build(backend): ben-manes.versions.gradle.plugin 0.48.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   id("com.diffplug.spotless") version "6.21.0"
-  id("com.github.ben-manes.versions") version "0.47.0"
+  id("com.github.ben-manes.versions") version "0.48.0"
   id("org.sonarqube") version "4.3.1.3277"
 }
 
